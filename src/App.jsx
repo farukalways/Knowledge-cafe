f05772a4577cfbc5cbfa8d4de6641fd2d5a26904
farukalways <alishopon367@gmail.com>
2024-10-22 07:37:03 +0600

    bookmark button add and author profile besides padding add

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import Blogs from './Component/Blogs/blogs'
 import BookMarks from './Component/Bookmarks/BookMarks'
@@ -5,14 +6,18 @@ import Header from './Component/Header/Header'
 
 function App() {
 
+  const [Bookmarks, setbookmarks] = useState([]);
+  const handleAddToBookmark = (blog) => {
+     console.log('Bookmarks Adding soon');
+  }
   return (
     <>
       <Header></Header>
 
-      <main className='md:flex'>
+      <main className='md:flex max-w-7xl mx-auto'>
 
-      <Blogs></Blogs>
-      <BookMarks></BookMarks>
+        <Blogs handleAddToBookmark={handleAddToBookmark}></Blogs>
+        <BookMarks></BookMarks>
 
       </main>
     </>
--- src/Component/Blog/Blog.jsx
+++ src/Component/Blog/Blog.jsx
@@ -1,27 +1,34 @@
 import PropTypes from 'prop-types';
+import { CiBookmark } from "react-icons/ci";
 
-const Blog = ({ blog }) => {
-  const {title, cover, author, author_img, posted_date, reading_time, hashtags} = blog;
+const Blog = ({ blog , handleAddToBookmark}) => {
+  const { title, cover, author, author_img, posted_date, reading_time, hashtags } = blog;
   return (
-    <div>
-      <img src={cover} alt="" />
-      <div className='flex justify-between'>
+    <div className='mb-20'>
+      <img className='w-full mb-8' src={cover} alt="" />
+      <div className='flex justify-between mb-4'>
         <div className='flex'>
-            <img className='w-14' src={author_img} alt="" />
-            <div className='ml-6'>
-                <h3 className='text-2xl'> {author}</h3>
-                <p>{posted_date}</p>
-            </div>
+          <img className='w-14' src={author_img} alt="" />
+          <div className='ml-6'>
+            <h3 className='text-2xl'> {author}</h3>
+            <p>{posted_date}</p>
+          </div>
         </div>
 
         <div>
-            <span>{reading_time} min read</span>
+          <span>{reading_time} min read</span>
+          <button
+          onClick={handleAddToBookmark}
+            className='ml-2 text-2xl'
+          ><CiBookmark /></button>
         </div>
       </div>
       <h4 className="text-4xl">{title}</h4>
       <p>
         {
-          hashtags.map((hash, idx) => <span key={idx}><a href="">#{hash} </a></span>)
+          hashtags.map((hash, idx) => <span
+            key={idx}>
+            <a href="">#{hash} </a></span>)
         }
       </p>
     </div>
